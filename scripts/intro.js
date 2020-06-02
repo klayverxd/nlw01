e062dc22c18a1652cb89269af8576726052cd3ea
Klayver Ximenes <klayverx@gmail.com>
2020-06-02 12:55:08 -0300

    segunda aula

---
+++ scripts/intro.js
@@ -0,0 +1,69 @@
+// document.write('Hello')
+
+// variaveis, tipos de dados
+// var myvar = "hello"
+// const myconst = "He"
+
+// document.write(myvar + myvar)
+// document.write()
+
+// const s1 = "isso é uma string"
+// const s2 = 'isso também é uma string'
+// const s3 = ´isso é uma string também´
+
+//number 
+// const n1 = 1
+// const n2 = 12
+
+// document.white(n1 + n2)
+
+//boolean -> true ou false
+// const bTrue = true
+// const bFalse = false
+
+// document.write(bTrue)
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = 
+//   altura: "1,87m",
+//   idade: 24,
+//   solteiro: true,
+//   correr() {
+//     return 'correu'
+//   }
+// }
+
+// document.write(pessoa.correr())
+// pessoa.correr()
+
+//Array - vetor
+// const colecao = ["blue", 'green', 2, {name: "my name"}]
+
+// document.write(colecao[3].name)
+
+// Funções
+
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// sayMyName("Klayver")
+// sayMyName("Klaasd")
+// 
+// Condicionais
+
+// const notaFinal = 7
+
+// if(notaFinal < 5) {
+//   document.write("reprovado")
+// } else {
+//   document.write("aprovado")
+// }
+
+// Repetições
+
+// for(i = 0; i < 10; i++) {
+//   document.write(`<p> ${i} </p>`)
+// }
+
