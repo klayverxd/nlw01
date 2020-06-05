90d1c50343d186898ff49cf78cc146653f0b7922
Klayver Ximenes <klayverx@gmail.com>
2020-06-05 12:21:09 -0300

    quarta aula

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
-// "importando o express"
+// node.js - ambiente de execução do JS (servidor)
+// nodemon - atualiza o servidor(node), sem ser manualmente
+// express - framework do JS para estruturação web (ex.:Rotas)
+// template engine (nunjucks) - html dinâmico
+
+
+
+// importando o express (do node_modules)
 const express = require("express")
 // um objeto
 const server = express()
+
+// configurar pasta pública
+// configuração do servidor
+server.use(express.static("public"))
+
+// "pedindo" template engine (do node_modules)
+const nunjucks = require("nunjucks")
+// configurando o nunjucks mostrando a pagina inicial do projeto e passando o caminho do servidor e tirando o cache
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
 
 // configurar caminhos da aplicação
 // página inicial
@@ -9,7 +28,15 @@ const server = express()
 // res: Resposta
 server.get("/", (req, res) => {
     // variável global (diretório) concatenada com o caminho da pagina desejada
-    res.sendFile(__dirname + "/views/index.html")
+    // 0.1 - res.sendFile(__dirname + "/views/index.html")
+    // 0.2 - depois que já temos o nunjucks configurado ligado ao express
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    // variável global (diretório) concatenada com o caminho da pagina desejada
+    // 0.1 - res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
 
